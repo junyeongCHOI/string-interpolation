14e2242bda3d4cd7d501b09a163e0c41d920855b
junyeongCHOI <asxd153@gmail.com>
2022-12-02 15:10:04 +0900

    add parse test

--- test/parseInterpolation.spec.ts
+++ test/parseInterpolation.spec.ts
@@ -2,25 +2,29 @@ import { parseInterpolation } from "../src/index";
 
 describe("parseInterpolation", () => {
   it("should match single key", () => {
-    const parsed = parseInterpolation("test {{test || defaultValue}} test");
-    expect(parsed).toEqual(["test"]);
+    const parsed = parseInterpolation(
+      "test {{test || defaultValue}} test {{tags.test || defaultValue}}"
+    );
+    expect(parsed).toEqual(["test", "tags.test"]);
   });
 
   it("should match single key without defaultValue", () => {
-    const parsed = parseInterpolation("test {{test}} test");
-    expect(parsed).toEqual(["test"]);
+    const parsed = parseInterpolation("test {{test}} test {{tags.test}}");
+    expect(parsed).toEqual(["test", "tags.test"]);
   });
 
   it("should match multiple keys", () => {
-    const parsed = parseInterpolation("test {{test || defaultValue}} {{test}}");
-    expect(parsed).toEqual(["test"]);
+    const parsed = parseInterpolation(
+      "test {{test || defaultValue}} {{test}} {{tags.test}} {{tags.test}}"
+    );
+    expect(parsed).toEqual(["test", "tags.test"]);
   });
 
   it("should match multiple keys 2", () => {
     const parsed = parseInterpolation(
-      "test {{test || defaultValue}} {{test2 || defaultValue2}}"
+      "test {{test || defaultValue}} {{test2 || defaultValue2}} {{tags.test}} {{tags.test.test || defaultValue3}}"
     );
-    expect(parsed).toEqual(["test", "test2"]);
+    expect(parsed).toEqual(["test", "test2", "tags.test", "tags.test.test"]);
   });
 
   it("should match multiple keys without defaultValue", () => {
@@ -35,8 +39,8 @@ describe("parseInterpolation", () => {
 
   it("should match even if string has weird indent", () => {
     const parsed = parseInterpolation(
-      "test {{ test }} {{     default  ||   defaultValue     }} {{     test   }}"
+      "test {{ test }} {{     default  ||   defaultValue     }} {{     test   }} {{        tags.test }}"
     );
-    expect(parsed).toEqual(["test", "default"]);
+    expect(parsed).toEqual(["test", "default", "tags.test"]);
   });
 });
